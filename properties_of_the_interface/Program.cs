9592159b0df0b43c6057dbff6a3f308815f44e32
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-09 20:30:23 +1100

    Initial commit

---
+++ properties_of_the_interface/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  C# Program to Demonstrate Properties of the Interface
+ */
+
+using System;
+
+namespace properties_of_the_interface
+{
+
+    interface IValue
+    {
+        int Count { get; set; }
+        string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
